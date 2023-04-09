4ed79cffc9c1386cb2e068aadbdd52dc1bc05048
Sizakele Mahlangu <misssizakele@gmail.com>
2023-04-09 15:47:32 +0200

    Added functionality to all buttons

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function clearScreen() {
-    document.getElementById("result").value = "";
+    document.getElementById("display").value = "";
 }
 
-function display(value) {
-    document.getElementById("result").value += value;
+function displayVal(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("display").value;
+    var q = eval(p);
+    document.getElementById("display").value = q;
 }
